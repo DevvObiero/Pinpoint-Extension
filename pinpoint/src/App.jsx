35955fa2d995e3fd6bae81821c91dbd8747978dc
DevvObiero <obieropauldev@gmail.com>
2025-04-06 07:49:31 +0300

    I want to start working on the widget this morning

--- pinpoint/src/App.jsx
+++ pinpoint/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
       {/* Pin List */}
       {pins.length === 0 ? (
-        <p className="text-center text-gray-500 dark:text-gray-400">
+        <p className="text-center text-lataffa dark:text-gray-400">
           No pins yet
         </p>
       ) : (
