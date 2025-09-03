98e5bb14e60c53cc0ed2b48982dfb8cb6f61a2f3
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-09-03 14:58:58 +0300

    main.tx

---
+++ pinpoint/src/main.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+
+const root = document.getElementById("root");
+
+if (root) {
+  ReactDOM.createRoot(root as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found!");
+}
